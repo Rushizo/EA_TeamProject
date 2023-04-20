c48c03e4fb1cd9abb9d0bc0f3d57a53f7f4c99ae
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-04-20 12:40:59 +0200

    HO FINITO! ORA PIANGO!!!

--- JS/feature-footer.js
+++ JS/feature-footer.js
@@ -5,24 +5,24 @@ let isClicked2 = true;
 
 let showOrHide = function(){
     if (isClicked){
-    divContainer.style.display = "flex";
-    divContainer2.style.display = "none";
+    divContainer.style.visibility = "visible";
+    divContainer2.style.visibility = "hidden";
     isClicked = false;
     isClicked2 = true;
     } else {
-    divContainer.style.display = "none";
+    divContainer.style.visibility = "hidden";
     isClicked = true;
     }
 }
 
 let showOrHide2 = function(){
     if (isClicked2){
-    divContainer2.style.display = "flex";
-    divContainer.style.display = "none";
+    divContainer2.style.visibility = "visible";
+    divContainer.style.visibility = "hidden";
     isClicked2 = false;
     isClicked = true;
     } else {
-    divContainer2.style.display = "none";
+    divContainer2.style.visibility = "hidden";
     isClicked2 = true;
     }
 }

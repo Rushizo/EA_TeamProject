a8a769d1a623af22eaf9646af294a4f33fafc8c8
Danilo Innocente Carbone <danilocarbone2015@gmail.com>
2023-04-26 20:35:48 +0200

    fixed focus span on body2

--- JS/body2.js
+++ JS/body2.js
@@ -4,7 +4,7 @@ const $fifa = document.querySelector("#fifa");
 const $f1 = document.querySelector("#F1");
 const $apex = document.querySelector("#apex");
 const $theSims = document.querySelector("#the-sims");
-const $battlefild = document.querySelector("#battlefild");
+const $battlefield = document.querySelector("#battlefield");
 const $insideEa = document.querySelector("#inside-EA");
 
 const $container1 = document.querySelector(".container1");
@@ -31,6 +31,22 @@ $notizieEa.addEventListener("click", () => {
         $container6.style.display = "none";
         $container7.style.display = "none";
         $container8.style.display = "none";
+        $notizieEa.style.borderBottom = "1px solid red";
+        $eaPlay.style.borderBottom = "none";
+        $fifa.style.borderBottom = "none";
+        $f1.style.borderBottom = "none";
+        $apex.style.borderBottom = "none";
+        $theSims.style.borderBottom = "none";
+        $battlefield.style.borderBottom = "none";
+        $insideEa.style.borderBottom = "none";
+        $notizieEa.style.backgroundColor = "rgba(163, 162, 162, 0.3)";
+        $eaPlay.style.backgroundColor = "var(--page-color)";
+        $fifa.style.backgroundColor = "var(--page-color)";
+        $f1.style.backgroundColor = "var(--page-color)";
+        $apex.style.backgroundColor = "var(--page-color)";
+        $theSims.style.backgroundColor = "var(--page-color)";
+        $battlefield.style.backgroundColor= "var(--page-color)";
+        $insideEa.style.backgroundColor = "var(--page-color)";
     } else {
         
     }
@@ -40,14 +56,28 @@ $eaPlay.addEventListener("click", () => {
     if(clicked){
         $container1.style.display = "none";
         $container2.style.display = "block";
-        
         $container3.style.display = "none";
         $container4.style.display = "none";
         $container5.style.display = "none";
         $container6.style.display = "none";
         $container7.style.display = "none";
         $container8.style.display = "none";
-
+        $notizieEa.style.borderBottom = "none";
+        $eaPlay.style.borderBottom = "1px solid red";
+        $fifa.style.borderBottom = "none";
+        $f1.style.borderBottom = "none";
+        $apex.style.borderBottom = "none";
+        $theSims.style.borderBottom = "none";
+        $battlefield.style.borderBottom = "none";
+        $insideEa.style.borderBottom = "none";
+        $notizieEa.style.backgroundColor = "var(--page-color)";
+        $eaPlay.style.backgroundColor = "rgba(163, 162, 162, 0.3)";
+        $fifa.style.backgroundColor = "var(--page-color)";
+        $f1.style.backgroundColor = "var(--page-color)";
+        $apex.style.backgroundColor = "var(--page-color)";
+        $theSims.style.backgroundColor = "var(--page-color)";
+        $battlefield.style.backgroundColor = "var(--page-color)";
+        $insideEa.style.backgroundColor = "var(--page-color)";
     } else {
         
     }
@@ -63,6 +93,22 @@ $fifa.addEventListener("click", () => {
         $container6.style.display = "none";
         $container7.style.display = "none";
         $container8.style.display = "none";
+        $notizieEa.style.borderBottom = "none";
+        $eaPlay.style.borderBottom = "none";
+        $fifa.style.borderBottom = "1px solid red";
+        $f1.style.borderBottom = "none";
+        $apex.style.borderBottom = "none";
+        $theSims.style.borderBottom = "none";
+        $battlefield.style.borderBottom = "none";
+        $insideEa.style.borderBottom = "none";
+        $notizieEa.style.backgroundColor = "var(--page-color)";
+        $eaPlay.style.backgroundColor = "var(--page-color)";
+        $fifa.style.backgroundColor = "rgba(163, 162, 162, 0.3)";
+        $f1.style.backgroundColor = "var(--page-color)";
+        $apex.style.backgroundColor = "var(--page-color)";
+        $theSims.style.backgroundColor = "var(--page-color)";
+        $battlefield.style.backgroundColor = "var(--page-color)";
+        $insideEa.style.backgroundColor = "var(--page-color)";
     } else {
         
     }
@@ -79,6 +125,22 @@ $f1.addEventListener("click", () => {
         $container6.style.display = "none";
         $container7.style.display = "none";
         $container8.style.display = "none";
+        $notizieEa.style.borderBottom = "none";
+        $eaPlay.style.borderBottom = "none";
+        $fifa.style.borderBottom = "none";
+        $f1.style.borderBottom = "1px solid red";
+        $apex.style.borderBottom = "none";
+        $theSims.style.borderBottom = "none";
+        $battlefield.style.borderBottom = "none";
+        $insideEa.style.borderBottom = "none";
+        $notizieEa.style.backgroundColor = "var(--page-color)";
+        $eaPlay.style.backgroundColor = "var(--page-color)";
+        $fifa.style.backgroundColor = "var(--page-color)";
+        $f1.style.backgroundColor = "rgba(163, 162, 162, 0.3)";
+        $apex.style.backgroundColor = "var(--page-color)";
+        $theSims.style.backgroundColor = "var(--page-color)";
+        $battlefield.style.backgroundColor = "var(--page-color)";
+        $insideEa.style.backgroundColor = "var(--page-color)";
     } else {
         
     }
@@ -94,6 +156,22 @@ $apex.addEventListener("click", () => {
         $container6.style.display = "none";
         $container7.style.display = "none";
         $container8.style.display = "none";
+        $notizieEa.style.borderBottom = "none";
+        $eaPlay.style.borderBottom = "none";
+        $fifa.style.borderBottom = "none";
+        $f1.style.borderBottom = "none";
+        $apex.style.borderBottom = "1px solid red";
+        $theSims.style.borderBottom = "none";
+        $battlefield.style.borderBottom = "none";
+        $insideEa.style.borderBottom = "none";
+        $notizieEa.style.backgroundColor = "var(--page-color)";
+        $eaPlay.style.backgroundColor = "var(--page-color)";
+        $fifa.style.backgroundColor = "var(--page-color)";
+        $f1.style.backgroundColor = "var(--page-color)";
+        $apex.style.backgroundColor = "rgba(163, 162, 162, 0.3)";
+        $theSims.style.backgroundColor = "var(--page-color)";
+        $battlefield.style.backgroundColor = "var(--page-color)";
+        $insideEa.style.backgroundColor = "var(--page-color)";
     } else {
         
     }
@@ -110,12 +188,28 @@ $theSims.addEventListener("click", () => {
         $container6.style.display = "block";
         $container7.style.display = "none";
         $container8.style.display = "none";
+        $notizieEa.style.borderBottom = "none";
+        $eaPlay.style.borderBottom = "none";
+        $fifa.style.borderBottom = "none";
+        $f1.style.borderBottom = "none";
+        $apex.style.borderBottom = "none";
+        $theSims.style.borderBottom = "1px solid red";
+        $battlefield.style.borderBottom = "none";
+        $insideEa.style.borderBottom = "none";
+        $notizieEa.style.backgroundColor = "var(--page-color)";
+        $eaPlay.style.backgroundColor = "var(--page-color)";
+        $fifa.style.backgroundColor = "var(--page-color)";
+        $f1.style.backgroundColor = "var(--page-color)";
+        $apex.style.backgroundColor = "var(--page-color)";
+        $theSims.style.backgroundColor = "rgba(163, 162, 162, 0.3)";
+        $battlefield.style.backgroundColor = "var(--page-color)";
+        $insideEa.style.backgroundColor = "var(--page-color)";
     } else {
         
     }
 })
 
-$battlefild.addEventListener("click", () => {
+$battlefield.addEventListener("click", () => {
     if(clicked){
         $container1.style.display = "none";
         $container2.style.display = "none";
@@ -125,6 +219,22 @@ $battlefild.addEventListener("click", () => {
         $container6.style.display = "none";
         $container7.style.display = "block";
         $container8.style.display = "none";
+        $notizieEa.style.borderBottom = "none";
+        $eaPlay.style.borderBottom = "none";
+        $fifa.style.borderBottom = "none";
+        $f1.style.borderBottom = "none";
+        $apex.style.borderBottom = "none";
+        $theSims.style.borderBottom = "none";
+        $battlefield.style.borderBottom = "1px solid red";
+        $insideEa.style.borderBottom = "none";
+        $notizieEa.style.backgroundColor = "var(--page-color)";
+        $eaPlay.style.backgroundColor = "var(--page-color)";
+        $fifa.style.backgroundColor = "var(--page-color)";
+        $f1.style.backgroundColor = "var(--page-color)";
+        $apex.style.backgroundColor = "var(--page-color)";
+        $theSims.style.backgroundColor = "var(--page-color)";
+        $battlefield.style.backgroundColor = "rgba(163, 162, 162, 0.3)";
+        $insideEa.style.backgroundColor = "var(--page-color)";
     } else {
         
     }
@@ -140,6 +250,22 @@ $insideEa.addEventListener("click", () => {
         $container6.style.display = "none";
         $container7.style.display = "none";
         $container8.style.display = "block";
+        $notizieEa.style.borderBottom = "none";
+        $eaPlay.style.borderBottom = "none";
+        $fifa.style.borderBottom = "none";
+        $f1.style.borderBottom = "none";
+        $apex.style.borderBottom = "none";
+        $theSims.style.borderBottom = "none";
+        $battlefield.style.borderBottom = "none";
+        $insideEa.style.borderBottom = "1px solid red";
+        $notizieEa.style.backgroundColor = "var(--page-color)";
+        $eaPlay.style.backgroundColor = "var(--page-color)";
+        $fifa.style.backgroundColor = "var(--page-color)";
+        $f1.style.backgroundColor = "var(--page-color)";
+        $apex.style.backgroundColor = "var(--page-color)";
+        $theSims.style.backgroundColor = "var(--page-color)";
+        $battlefield.style.backgroundColor = "var(--page-color)";
+        $insideEa.style.backgroundColor = "rgba(163, 162, 162, 0.3)";
     } else {
         
     }
